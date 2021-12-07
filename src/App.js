cedfcfdb4e3358e327f4919d2785db998e2f4c08
yskim <yskim@builton.co.kr>
2021-12-07 15:03:42 +0900

    인피티니 스크롤 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [datas, setDatas] = useState([]);
+  const [fullHeightN, setFullHeightN] = useState(0);
+  const [childLength, setChildLength] = useState(30);
+  const fullContent = useRef();
+  const childContent = useRef();
+  const initialDatas = require("./json/infiniteScroll.json");
+
+  useEffect(() => {
+    setDatas(initialDatas.slice(0, childLength));
+  }, []);
+
+  function OnScroll(e) {
+    const scrollAreaHeight = fullContent.current.clientHeight;
+    const myScroll = e.target.scrollTop + scrollAreaHeight;
+    const childHeight = childContent.current.clientHeight;
+
+    if (myScroll > fullHeightN) {
+      setChildLength((prevState) => prevState + 30);
+      setFullHeightN(childHeight * childLength);
+      setDatas(initialDatas.slice(0, childLength));
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="scroll-container" onScroll={OnScroll} ref={fullContent}>
+        {datas?.map((data, index) => (
+          <div key={index} className="content-contaienr" ref={childContent}>
+            {data.title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
