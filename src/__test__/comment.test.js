968bd1efd46cc2c924a366bc9546ba3cc159c3ff
hanamDeveloper <zmf1118@gmail.com>
2021-12-27 14:45:02 +0900

    Add comment test case for remove comment

--- src/__test__/comment.test.js
+++ src/__test__/comment.test.js
@@ -1,25 +1,5 @@
 describe("show Test", () => {
-  const comment = [
-    {
-      id: 1,
-      userName: "zmf1111",
-      content: "1",
-    },
-    {
-      id: 2,
-      userName: "zmf1111",
-      content: "2",
-    },
-    {
-      id: 3,
-      userName: "zmf1111",
-      content: "3",
-    },
-  ];
-
-  it("should add comment", () => {
-    const inputWord = "4";
-    const expected = [
+    const comment = [
       {
         id: 1,
         userName: "zmf1111",
@@ -35,21 +15,62 @@ describe("show Test", () => {
         userName: "zmf1111",
         content: "3",
       },
-      {
-        id: 4,
-        userName: "zmf1111",
-        content: "4",
-      },
     ];
-    expect(addComment(comment, inputWord)).toEqual(expected);
-  });
-
-
-  const addComment = (comment, inputWord) => {
-    return comment.concat({
-      id: comment.length + 1,
-      content: inputWord,
-      userName: "zmf1111",
+  
+    it("should add comment", () => {
+      const inputWord = "4";
+      const expected = [
+        {
+          id: 1,
+          userName: "zmf1111",
+          content: "1",
+        },
+        {
+          id: 2,
+          userName: "zmf1111",
+          content: "2",
+        },
+        {
+          id: 3,
+          userName: "zmf1111",
+          content: "3",
+        },
+        {
+          id: 4,
+          userName: "zmf1111",
+          content: "4",
+        },
+      ];
+      expect(addComment(comment, inputWord)).toEqual(expected);
     });
-  };
-});
+  
+    it("should remove last comment", () => {
+      const id = 2;
+      const expected = [
+        {
+          id: 1,
+          userName: "zmf1111",
+          content: "1",
+        },
+        {
+          id: 3,
+          userName: "zmf1111",
+          content: "3",
+        },
+      ];
+      expect(removeComment(comment, id)).toEqual(expected);
+    });
+  
+    const addComment = (comments, inputWord) => {
+        return comment.concat({
+          id: comments.length + 1,
+          content: inputWord,
+          userName: "zmf1111",
+        });
+      };
+
+      const removeComment = (comments, id) => {
+        return comments.filter(comment => comment.id !== id)
+      };
+  });
+  
