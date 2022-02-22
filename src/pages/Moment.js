b58118cf77a47322c13601332a1fe39d5cf9579c
hanamDeveloper <zmf1118@gmail.com>
2022-02-23 00:10:53 +0900

    Add 윤년

--- src/pages/Moment.js
+++ src/pages/Moment.js
@@ -22,6 +22,14 @@ export default function MomentExample() {
                 2018년 3월 10일 13시에 하루 더하기:
                 {moment.tz("2018-03-10 13:00:00", "America/New_York").add(24, "hour").format()}
             </div>
+            <div>
+                2017년 1월 1일에 1년 빼기:
+                {moment("2017-01-01").subtract(1, "year").format()}
+            </div>
+            <div>
+                2017년 1월 1일에 365일 빼기 더하기:
+                {moment("2017-01-01").subtract(365, "day").format()}
+            </div>
         </div>
     )
 }
